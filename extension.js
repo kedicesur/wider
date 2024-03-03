5495b01cf491fbcc2663ba6318cc712a1f2e0e6b
Ömer Kaşdarma <omer.kasdarma@gmail.com>
2024-03-04 01:42:38 +0300

    fix: when object returning expression sequences      (comma first type paranthesised code blocks)      are chained the dot "." gets the opening left      paranthesis index.

--- extension.js
+++ extension.js
@@ -151,9 +151,10 @@ function activate(context) {
     }
     return !cnt ? mod === "." ? ( dix = txt.lastIndexOf(".", pch)                    // get the index of dot "." of the last method before matching left paranthesis
                                 , txt.substring(dix,pch)                             // normally this is the method name but if the matching left paren belongs to
-                                     .search(/\(/) >= 0 ? [pch, false, false]        // an expression sequenced code block in a callback that returns a promise etc
-                                                        : [dix, false, false]        // and we like to chain to it with a ".then" this test prevents it to catch the
-                                )                                                    // dot at dix index and uses the matching left paranthesis index instead.
+                                     .search(/\(/) >= 0 ||                           // an expression sequenced code block in a callback that returns a promise etc
+                                  dix === -1            ? [pch, false, false]        // and we like to chain to it with a ".then" this test prevents it to catch the
+                                                        : [dix, false, false]        // dot at dix index and uses the matching left paranthesis index instead.
+                                )
                               :
                   mod === ";" ? dix >= 0 ? [-1, new vscode.Position(pln,dix), false]
                                          : [-1, false, false]
